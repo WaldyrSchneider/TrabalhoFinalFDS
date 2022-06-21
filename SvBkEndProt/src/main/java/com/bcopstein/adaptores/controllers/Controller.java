c76253030aa8b9851a6fe40206f1e1d629a4a82e
Waldyr Schneider <waldyr.schneider@edu.pucrs.br>
2022-06-21 11:13:30 -0300

    refatorando

--- SvBkEndProt/src/main/java/com/bcopstein/adaptores/controllers/Controller.java
+++ SvBkEndProt/src/main/java/com/bcopstein/adaptores/controllers/Controller.java
@@ -25,7 +25,7 @@ import com.bcopstein.negocio.repositorios.IProdutoRepositorio;
 @RequestMapping("/vendas")
 public class Controller {
   private final List<String> vendasEfetuadas;
-  private final Map<String,Integer> cacheFrete; 
+  private final Map<String, Integer> cacheFrete;
   private final IProdutoRepositorio iProdutoRepositorio;
 
   @Autowired
@@ -34,7 +34,6 @@ public class Controller {
     this.iProdutoRepositorio = iProdutoRepositorio;
     // Cria cache frete
     cacheFrete = new HashMap<>();
-
 
     // Cria a lista de vendas efetuadas
     vendasEfetuadas = new ArrayList<>();
@@ -48,10 +47,8 @@ public class Controller {
 
   // @GetMapping("/autorizacao")
   // @CrossOrigin(origins = "*")
-  // public boolean podeVender(@RequestParam final Integer codProd,
-  //                           @RequestParam final Integer qtdade) {
-  //   final boolean disponivel =
-  //       produtos.stream().anyMatch(p -> p.getCodigo() == codProd && p.getQtdade() >= qtdade);
+  // public boolean podeVender(@RequestParam final Integer codProd, @RequestParam final Integer qtdade) {
+  //   final boolean disponivel = produtos.stream().anyMatch(p -> p.getCodigo() == codProd && p.getQtdade() >= qtdade);
   //   return disponivel;
   // }
 
@@ -59,83 +56,88 @@ public class Controller {
   // @CrossOrigin(origins = "*")
   // public boolean confirmaVenda(@RequestBody final ItemCarrinho[] itens) {
 
-  //   ArrayList<Produto> listaProdutos = new ArrayList<>();
-  //   ArrayList<Integer> listaQtdades = new ArrayList<>();
+  // ArrayList<Produto> listaProdutos = new ArrayList<>();
+  // ArrayList<Integer> listaQtdades = new ArrayList<>();
 
-  //   for (ItemCarrinho item : itens) {
-  //     final Produto produto =
-  //         produtos.stream().filter(p -> p.getCodigo() == item.getCodigo()).findAny().orElse(null);
+  // for (ItemCarrinho item : itens) {
+  // final Produto produto =
+  // produtos.stream().filter(p -> p.getCodigo() ==
+  // item.getCodigo()).findAny().orElse(null);
 
-  //     if (produto == null) {
-  //       return false;
-  //     }
+  // if (produto == null) {
+  // return false;
+  // }
 
-  //     listaQtdades.add(item.getQuantidade());
-  //     listaProdutos.add(produto);
-  //   }
+  // listaQtdades.add(item.getQuantidade());
+  // listaProdutos.add(produto);
+  // }
 
-  //   StringBuilder builder = new StringBuilder();
+  // StringBuilder builder = new StringBuilder();
 
-  //   for (int i = 0; i < listaProdutos.size(); i++) {
-  //     final Produto produto = listaProdutos.get(i);
-  //     final int qtdade = listaQtdades.get(i);
-  //     produto.saidaDeProduto(qtdade);
+  // for (int i = 0; i < listaProdutos.size(); i++) {
+  // final Produto produto = listaProdutos.get(i);
+  // final int qtdade = listaQtdades.get(i);
+  // produto.saidaDeProduto(qtdade);
 
-  //     builder.append(produto.getCodigo());
-  //     builder.append(" ");
-  //     builder.append(produto.getDescricao());
-  //     builder.append(" ");
-  //     builder.append(qtdade);
-  //     builder.append("\n");
-  //   }
+  // builder.append(produto.getCodigo());
+  // builder.append(" ");
+  // builder.append(produto.getDescricao());
+  // builder.append(" ");
+  // builder.append(qtdade);
+  // builder.append("\n");
+  // }
 
-  //   vendasEfetuadas.add(builder.toString());
-  //   return true;
+  // vendasEfetuadas.add(builder.toString());
+  // return true;
   // }
 
   // @GetMapping("/historico")
   // @CrossOrigin(origins = "*")
   // public List<String> vendasEfetuadas() {
-  //   return vendasEfetuadas;
+  // return vendasEfetuadas;
   // }
 
   // @PostMapping("/subtotal")
   // @CrossOrigin(origins = "*")
-  // public Integer[] calculaSubtotal(@RequestBody final ParamSubtotal_DTO param) {
-  //   Integer subtotal = 0;
-  //   Integer imposto = 0;
+  // public Integer[] calculaSubtotal(@RequestBody final ParamSubtotal_DTO param)
+  // {
+  // Integer subtotal = 0;
+  // Integer imposto = 0;
 
-  //   System.out.println(param.getEndereco());
+  // System.out.println(param.getEndereco());
 
-  //   // Verifica se o endereço é invalido
-  //   if (param.getEndereco() == null || 
-  //       param.getEndereco().isEmpty() ||
-  //       param.getEndereco().isBlank()){
-  //       throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Endereco invalido");
-  //   }
-  //   // Verifica se o endereço já está na cache
-  //   if (!cacheFrete.keySet().contains(param.getEndereco())){
-  //     // Calcula o frete 
-  //     cacheFrete.put(param.getEndereco(),25);
-  //   }
+  // // Verifica se o endereço é invalido
+  // if (param.getEndereco() == null ||
+  // param.getEndereco().isEmpty() ||
+  // param.getEndereco().isBlank()){
+  // throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Endereco
+  // invalido");
+  // }
+  // // Verifica se o endereço já está na cache
+  // if (!cacheFrete.keySet().contains(param.getEndereco())){
+  // // Calcula o frete
+  // cacheFrete.put(param.getEndereco(),25);
+  // }
 
-  //   for (final ItemCarrinho it : param.getItens()) {
-  //     // Procurar o produto pelo código
-  //     final Produto prod =
-  //         produtos.stream().filter(p -> p.getCodigo() == it.getCodigo()).findAny().orElse(null);
+  // for (final ItemCarrinho it : param.getItens()) {
+  // // Procurar o produto pelo código
+  // final Produto prod =
+  // produtos.stream().filter(p -> p.getCodigo() ==
+  // it.getCodigo()).findAny().orElse(null);
 
-  //     if (prod != null) {
-  //       subtotal += (int) (prod.getPreco() * it.getQuantidade());
-  //     } else {
-  //       throw new IllegalArgumentException("Codigo invalido");
-  //     }
-  //   }
-  //   imposto = (int) (subtotal * 0.1);
-  //   final Integer[] resp = new Integer[4];
-  //   resp[0] = subtotal;
-  //   resp[1] = imposto;
-  //   resp[2] = subtotal + imposto;
-  //   resp[3] = cacheFrete.get(param.getEndereco()); // Frete: custo fixo por enquanto
-  //   return resp;
+  // if (prod != null) {
+  // subtotal += (int) (prod.getPreco() * it.getQuantidade());
+  // } else {
+  // throw new IllegalArgumentException("Codigo invalido");
+  // }
+  // }
+  // imposto = (int) (subtotal * 0.1);
+  // final Integer[] resp = new Integer[4];
+  // resp[0] = subtotal;
+  // resp[1] = imposto;
+  // resp[2] = subtotal + imposto;
+  // resp[3] = cacheFrete.get(param.getEndereco()); // Frete: custo fixo por
+  // enquanto
+  // return resp;
   // }
 }
